42b8e275b4e6e5dbb64226b96bcfd25abfb14c29
uthmane <45245804+uthmane@users.noreply.github.com>
2019-01-31 17:17:08 +0000

    Add files via upload
    This is a useful powershell script that maps a specific letter of the alphabet as a mount point for Google Drive File Stream. E.g. instead of using the default (G:), you can map the drive to - for example - K:

---
+++ GDrive.ps1
@@ -0,0 +1,12 @@
+﻿$registryPath = "HKLM:\SOFTWARE\Google\DriveFS"
+$Name = "DefaultMountPoint"
+$value = "B"
+
+IF(!(Test-Path $registryPath))
+    {
+    New-Item -Path $registryPath -Force | Out-Null
+    New-ItemProperty -Path $registryPath -Name $Name -Value $value`
+    -PropertyType DWORD -Force | Out-Null}
+ELSE{
+New-ItemProperty -Path $registryPath -Name $Name -Value $value`
+-PropertyType DWORD -Force | Out-Null}
